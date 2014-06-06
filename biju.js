dae5a5af51b186ac6e39f4d45d88ec4a2378a75c
Raphael Ivan <raphaivan@gmail.com>
2014-06-06 20:34:37 -0300

    Open file and add method

--- biju.js
+++ biju.js
@@ -1,50 +1,84 @@
 #!/usr/bin/env node
 require('./date.js');
-
 ;(function(){
-  function Biju () {
+  'use strict'
+
+  var fs = require('fs');
+
+  /* create Biju Object */
+  var Biju = {
+    file: 'list.txt'
+  };
+
+  /* Define the initializer method */
+  Biju.init = function (file) {
     var
         args = process.argv.slice(2)
       , method = args[0];
 
+    if (!file) {
+      Biju.file = 'list.txt';
+
+    };
+
     callMethod(method);
+  }
 
-    function  showOptions () {
-      console.log(
-        "==================================\n",
-        "add('task name', '03/06/2014') \n",
-        "remove('task name')"
-      )
+
+  function  showOptions () {
+    console.log(
+      "==================================\n",
+      "add('task name', '03/06/2014') \n",
+      "remove('task name')"
+    )
+  }
+
+  function callMethod (method) {
+    if (Biju.hasOwnProperty(method)) {
+      Biju[method].call();
+    } else {
+      showOptions();
     }
+  }
 
-
-    function callMethod (method) {
-      if (Biju.hasOwnProperty(method)) {
-        Biju[method].apply(args);
-      } else {
-        showOptions();
-      }
-    }
-  };
-
-  Biju.add = function (info) {
+  Biju.add = function () {
     var
         args = process.argv.slice(2)
-      , method = args[0]
-      , task = args[1]
-      , date = args[2];
+      , note = args[1]
+      , date = args[2]
+      , file = Biju.file;
 
     if (!date) {
       var date = new Date().format();
     };
 
-    console.log(args);
+    fs.writeFile(
+        file
+      , (note + ':' + date + '\n')
+      , function (err) {
+        if (err) {
+          return console.log('OOPS!')
+        };
+
+        console.log('Note saved!')
+      }
+    )
   };
 
 
   Biju.list = function () {
+    var
+      file = Biju.file;
+
+    fs.readFile(file, function(err, buffer){
+      var data = buffer.toString();
+
+      console.log(data);
+    })
   };
 
   Biju.remove = function  ()  {
   };
+
+  delete Biju.init();
 })();
